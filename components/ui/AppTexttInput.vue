6ae61f67c577836c2f4f000c97c16004dabdb690
afrasiabi1374 <mohammad.aligiga@gmail.com>
2022-04-07 13:47:27 +0430

    v-model-component

---
+++ components/ui/AppTexttInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-bind="$attrs" :value="value" v-on="{...$listeners, }" @input="input" type="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'APPTEXTINPUT',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    input (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/form.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>form</h1>
+    <label>firstName:</label>
+    <app-textt-input v-model="firstName"/>
+  </div>
+</template>
+
+<script>
+import AppTexttInput from '~/components/ui/AppTexttInput.vue'
+export default {
+  name: 'INDEX',
+  components: { AppTexttInput },
+  data () {
+    return {
+      firstName: ''
+    }
+  },
+  watch: {
+    firstName (value) {
+      console.log('firstName is :', value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
